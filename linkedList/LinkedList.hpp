8244653d040ef1d6655e798b4b7fdead59732798
Isaac Spiegel <isaacspiegel@gmail.com>
2021-02-14 23:07:47 -0600

    Fixed size

--- linkedList/LinkedList.hpp
+++ linkedList/LinkedList.hpp
@@ -32,7 +32,14 @@ int LinkedList<T>::size() const
 	/** TODO 
 		Fix this method
 	*/
-	return(0);
+	int count = 0;
+	Node<T>* temp = m_front;
+	while( temp != nullptr)
+	{
+		count += 1;
+		temp = temp->getNext();
+	}
+	return(count);
 }
 
 template <typename T>
